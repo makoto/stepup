23a429aac01f363cd837ba85d1201d19ee03bb96
Makoto Inoue <inouemak@googlemail.com>
2013-08-10 19:25:31 +0100

    Spike commit

--- static/js/App.js
+++ static/js/App.js
@@ -1,4 +1,35 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
-  $.get("/data/segments.json").done(function(d){console.log(d)})
+    $.get("/data/segments.json").done(function(segments){
+      var segment = segments.shift()
+
+      $('#play').on("click", function(evt){
+        pop = Popcorn("#video");
+        pop.on("timeupdate", function(){
+          $('#current-time').html(pop.currentTime())
+        })
+        pop.cue( segment.end_at, function() {
+          this.currentTime( segment.start_at );
+        });
+        pop.currentTime( segment.start_at );
+        pop.play();
+      })
+
+      $('#stop').on("click", function(evt){
+        pop.pause()
+      })
+
+      $('#reset').on("click", function(evt){
+        pop.currentTime(0)
+        console.log(pop.currentTime)
+      })
+
+      $('#next').on("click", function(evt){
+        segment = segments.shift()
+        pop.cue( segment.end_at, function() {
+          this.currentTime( segment.start_at );
+        });
+        pop.currentTime( segment.start_at );
+        pop.play();
+      })
+    })
 });
