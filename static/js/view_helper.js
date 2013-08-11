88e3918e14af4df8c2859c3b825bb3f45347c01c
Makoto Inoue <inouemak@googlemail.com>
2013-08-11 08:37:46 +0100

    Added view helper

---
+++ static/js/view_helper.js
@@ -0,0 +1,17 @@
+var ViewHelper = {
+  formatTime: function(time){
+    var total = Math.floor(parseFloat(time, 10)),
+        minutes = Math.floor(total / 60),
+        seconds = total - (60 * minutes)
+
+    var pad = function (n) {
+      var length = ("" + n).length
+      if (length == 2) {
+        return n
+      } else {
+        return "0" + n
+      }
+    }
+    return [pad(minutes), pad(seconds)].join(':')
+  }
+}
