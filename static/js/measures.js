e64efc09ed2dfb948d5650700e31ea8d41ec8f77
Craig <decoy9697@gmail.com>
2013-08-11 10:35:04 +0100

    New naming for levels / steps in CSS

--- static/js/measures.js
+++ static/js/measures.js
@@ -19,34 +19,36 @@
         },
 
         initialize: function() {
+            console.log('measuresview init');
             this.model.on('change', _.bind(this.selectCell, this));
         },
 
         onClickButton: function(e) {
+            console.log('click');
 
             var $button = $(e.target);
 
             this.model.set({
                 step: $button.data('step'),
-                position: $button.data('position')
+                level: $button.data('level')
             });
 
         },
 
         selectCell: function(opts) {
 
-            if( !opts.changed.step || !opts.changed.position) { return; }
+            if( !opts.changed.step || !opts.changed.level) { return; }
 
             console.log(opts.changed);
-            this.$el.find('.selected-row').removeClass('selected-row');
-            this.$el.find('.selected-measure').removeClass('selected-measure');
+            this.$el.find('.selected-level').removeClass('selected-level');
+            this.$el.find('.selected-step').removeClass('selected-step');
 
             var changed = opts.changed,
-                $row    = this.$el.find('.step-' + changed.step ),
-                $button = $row.find('[data-position=' + changed.position + ']');
+                $level  = this.$el.find('.level-' + changed.level ),
+                $button = $level.find('[data-step=' + changed.step + ']');
 
-            $row.addClass('selected-row');
-            $button.parent('li').addClass('.selected-measure');
+            $level.addClass('selected-level');
+            $button.parent('li').addClass('selected-step');
 
         }
 
