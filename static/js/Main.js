803b30f791e0782dff911097c2d8cfa09c21368b
Makoto Inoue <inouemak@googlemail.com>
2013-08-11 14:47:14 +0100

    Added naive "up"

--- static/js/Main.js
+++ static/js/Main.js
@@ -37,6 +37,37 @@ $( document ).ready(function() {
         pop.play();
       })
 
+      $('#up').on("click", function(evt){
+        var next_level_id = video.get("level") + 1
+        var levels = steps.current().get("levels")
+        var next_level = levels.filter(function(l){
+          return l.id == next_level_id
+        })[0]
+
+        var start_at = steps.findWhere({id:next_level.start_step_id}).get("start_at")
+        var end_at = steps.findWhere({id:next_level.end_step_id}).get("end_at")
+
+
+        video.set({"step":1, "level":next_level_id})
+
+        $('#from').html(App.ViewHelper.formatTime(start_at))
+        $('#to').html(App.ViewHelper.formatTime(end_at))
+
+        pop.destroy();
+        pop = Popcorn("#video");
+        pop.on("timeupdate", function(){
+          $('#current-time').html(App.ViewHelper.formatTime(pop.currentTime()))
+        })
+
+        pop.cue( end_at, function() {
+          this.currentTime( start_at );
+        });
+        pop.currentTime( start_at);
+        pop.play();
+        console.log('time',pop.currentTime())
+      })
+
+
     });
     steps.fetch()
 
